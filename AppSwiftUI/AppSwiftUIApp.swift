0bcc2e08354d458dd8a97c9ede534d1fd4dc925a
Jeffrey Manalang <jeffrey.manalang03@gmail.com>
2023-07-01 16:01:12 +0800

    Initial Commit
    Moving files for my initial start

---
+++ AppSwiftUI/AppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppSwiftUIApp.swift
+//  AppSwiftUI
+//
+//  Created by Jeffrey Manalang on 7/1/23.
+//
+
+import SwiftUI
+
+@main
+struct AppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppSwiftUI
+//
+//  Created by Jeffrey Manalang on 7/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
